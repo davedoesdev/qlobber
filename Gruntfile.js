6944e9fd9f01abd5462445b499bbfdca496110f6
David Halls <dahalls@gmail.com>
2017-07-05 07:02:51 +0100

    Switch to grunt-exec

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,37 +23,37 @@ module.exports = function (grunt)
             extraHeadingLevels: 1
         },
 
-        shell: {
+        exec: {
             cover: {
-                command: "./node_modules/.bin/nyc -x Gruntfile.js -x 'test/**' ./node_modules/.bin/grunt test"
+                cmd: "./node_modules/.bin/nyc -x Gruntfile.js -x 'test/**' ./node_modules/.bin/grunt test"
             },
 
             cover_report: {
-                command: './node_modules/.bin/nyc report -r lcov'
+                cmd: './node_modules/.bin/nyc report -r lcov'
             },
 
             cover_check: {
-                command: './node_modules/.bin/nyc check-coverage --statements 100 --branches 100 --functions 100 --lines 100'
+                cmd: './node_modules/.bin/nyc check-coverage --statements 100 --branches 100 --functions 100 --lines 100'
             },
 
             coveralls: {
-                command: 'cat coverage/lcov.info | coveralls'
+                cmd: 'cat coverage/lcov.info | coveralls'
             },
 
             bench: {
-                command: './node_modules/.bin/bench -c 20000 -i bench/options/default.js,bench/options/dedup.js -k options bench/add_match_remove bench/match'
+                cmd: './node_modules/.bin/bench -c 20000 -i bench/options/default.js,bench/options/dedup.js -k options bench/add_match_remove bench/match'
             },
 
             'bench-check': {
-                command: './node_modules/.bin/bench -c 20000 -i bench/options/check.js,bench/options/check-dedup.js -k options bench/add_match_remove bench/match'
+                cmd: './node_modules/.bin/bench -c 20000 -i bench/options/check.js,bench/options/check-dedup.js -k options bench/add_match_remove bench/match'
             },
 
             'bench-add-many': {
-                command: './node_modules/.bin/bench -c 1 -i bench/options/default.js,bench/options/dedup.js -k options bench/add_many.js'
+                cmd: './node_modules/.bin/bench -c 1 -i bench/options/default.js,bench/options/dedup.js -k options bench/add_many.js'
             },
 
             'bench-match-many': {
-                command: './node_modules/.bin/bench -c 1 -i bench/options/default.js,bench/options/dedup.js -k options bench/match_many.js'
+                cmd: './node_modules/.bin/bench -c 1 -i bench/options/default.js,bench/options/dedup.js -k options bench/match_many.js'
             }
         }
     });
@@ -61,20 +61,20 @@ module.exports = function (grunt)
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-apidox');
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('lint', 'jshint');
     grunt.registerTask('test', 'mochaTest');
     grunt.registerTask('docs', 'apidox');
-    grunt.registerTask('coverage', ['shell:cover',
-                                    'shell:cover_report',
-                                    'shell:cover_check']);
-    grunt.registerTask('coveralls', 'shell:coveralls');
-    grunt.registerTask('bench', ['shell:bench',
-                                 'shell:bench-add-many',
-                                 'shell:bench-match-many']);
-    grunt.registerTask('bench-check', 'shell:bench-check');
-    grunt.registerTask('bench-add-many', 'shell:bench-add-many');
-    grunt.registerTask('bench-match-many', 'shell:bench-match-many');
+    grunt.registerTask('coverage', ['exec:cover',
+                                    'exec:cover_report',
+                                    'exec:cover_check']);
+    grunt.registerTask('coveralls', 'exec:coveralls');
+    grunt.registerTask('bench', ['exec:bench',
+                                 'exec:bench-add-many',
+                                 'exec:bench-match-many']);
+    grunt.registerTask('bench-check', 'exec:bench-check');
+    grunt.registerTask('bench-add-many', 'exec:bench-add-many');
+    grunt.registerTask('bench-match-many', 'exec:bench-match-many');
     grunt.registerTask('default', ['jshint', 'mochaTest']);
 };
