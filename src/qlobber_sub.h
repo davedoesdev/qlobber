ea9afa6947ca01bdd92610b63af9560040e719ca
David Halls <dahalls@gmail.com>
2019-10-29 21:22:25 +0000

    Can use env from result instead of passing it as context

--- src/qlobber_sub.h
+++ src/qlobber_sub.h
@@ -3,24 +3,19 @@
 #include "qlobber_js_base.h"
 #include "js_options.h"
 
-struct TopicAndEnv {
-    std::optional<const std::string> topic;
-    Napi::Env env;
-};
-
 class QlobberSub :
-    public QlobberSubBase<Napi::Array, const TopicAndEnv>,
+    public QlobberSubBase<Napi::Array, const std::optional<const std::string>>,
     public Napi::ObjectWrap<QlobberSub> {
 public:
     QlobberSub(const Napi::CallbackInfo& info) :
-        QlobberSubBase<Napi::Array, const TopicAndEnv>(JSOptions(info)), 
+        QlobberSubBase<Napi::Array, const std::optional<const std::string>>(JSOptions(info)), 
         Napi::ObjectWrap<QlobberSub>(info) {}
 
     virtual ~QlobberSub() {}
 
     Napi::Value Add(const Napi::CallbackInfo& info) {
-        auto topic = info[0].As<Napi::String>();
-        auto val = info[1].As<Napi::Object>();
+        const auto topic = info[0].As<Napi::String>();
+        const auto val = info[1].As<Napi::Object>();
         add(topic, {
             val.Get("clientId").As<Napi::String>(),
             val.Get("topic").As<Napi::String>(),
@@ -30,31 +25,28 @@ public:
     }
 
     Napi::Value Remove(const Napi::CallbackInfo& info) {
-        auto topic = info[0].As<Napi::String>();
+        const auto topic = info[0].As<Napi::String>();
         if (info.Length() == 1) {
             remove(topic, std::nullopt);
         } else {
-            auto val = info[1].As<Napi::Object>();
+            const auto val = info[1].As<Napi::Object>();
             remove(topic, val.Get("clientId").As<Napi::String>());
         }
         return info.This();
     }
 
     Napi::Value Match(const Napi::CallbackInfo& info) {
-        auto topic = info[0].As<Napi::String>();
+        const auto topic = info[0].As<Napi::String>();
         auto r = Napi::Array::New(info.Env());
-        match(r, topic, {
-            info.Length() == 1 ?
-                std::nullopt :
-                std::optional<std::string>(info[1].As<Napi::String>()),
-            info.Env()
-        });
+        match(r, topic, info.Length() == 1 ?
+            std::nullopt :
+            std::optional<std::string>(info[1].As<Napi::String>()));
         return r;
     }
 
     Napi::Value Test(const Napi::CallbackInfo& info) {
-        auto topic = info[0].As<Napi::String>();
-        auto val = info[1].As<Napi::Object>();
+        const auto topic = info[0].As<Napi::String>();
+        const auto val = info[1].As<Napi::Object>();
         return Napi::Boolean::New(info.Env(), test(topic, {
             val.Get("clientId").As<Napi::String>(),
             val.Get("topic").As<Napi::String>()
@@ -73,18 +65,18 @@ public:
 private:
     void add_values(Napi::Array& dest,
                     const SubStorage& existing,
-                    const TopicAndEnv& context) override {
-        if (!context.topic) {
+                    const std::optional<const std::string>& topic) override {
+        if (!topic) {
             for (const auto& clientIdAndQos : existing.clientMap) {
-                Napi::Object obj = Napi::Object::New(context.env);
+                Napi::Object obj = Napi::Object::New(dest.Env());
                 obj.Set("clientId", clientIdAndQos.first);
                 obj.Set("topic", existing.topic);
                 obj.Set("qos", static_cast<uint32_t>(clientIdAndQos.second));
                 dest.Set(dest.Length(), obj);
             }
-        } else if (existing.topic == context.topic.value()) {
+        } else if (existing.topic == topic.value()) {
             for (const auto& clientIdAndQos : existing.clientMap) {
-                Napi::Object obj = Napi::Object::New(context.env);
+                Napi::Object obj = Napi::Object::New(dest.Env());
                 obj.Set("clientId", clientIdAndQos.first);
                 obj.Set("qos", static_cast<uint32_t>(clientIdAndQos.second));
                 dest.Set(dest.Length(), obj);
